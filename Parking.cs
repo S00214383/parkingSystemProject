0e95180d45d0e02f100934aaa97e19fe19aa9c9e
Rudy <rudgerylopes@gmail.com>
2025-03-24 19:52:19 +0000

    adding parking class

---
+++ Parking.cs
@@ -0,0 +1,41 @@
+﻿
+//instantiating  Parking class 
+using System;
+
+Parking park = new Parking();
+
+//
+bool stopping = true;
+do
+{
+    Console.WriteLine(" **** PARKING SYSTEM **** ");
+
+    Console.WriteLine("please enter your option: ");
+    Console.WriteLine(" 1 - Add a car");
+    Console.WriteLine(" 2 - Remove a car");
+    Console.WriteLine(" 3 - List cars");
+    Console.WriteLine(" 4 - Finish");
+
+
+
+    string optionSelected = Console.ReadLine();
+    switch (optionSelected)
+    {
+        case "1":
+            park.AddCar();
+
+            break;
+        case "2":
+            park.RemoveCar();
+
+            break;
+        case "3":
+            park.DisplayCars();
+
+            break;
+        case "4":
+            Console.WriteLine("Finish program!");
+            break;
+    }
+
+} while (stopping != false);
--- Program.cs
+++ Program.cs
@@ -1,8 +1,72 @@
 ﻿using System;
-
-public class Program
+public class Parking
 {
-	public Program()
-	{
-	}
+
+
+
+    List<string> vehicles = new List<string>();
+
+
+
+
+    //declaring methods
+    public void AddCar()
+    {
+        Console.WriteLine("Enter reg number to Add car: ");
+        string regNumber = Console.ReadLine();
+
+        vehicles.Add(regNumber);
+        Console.WriteLine($"car {regNumber} added to the system !");
+
+
+    }
+
+    public void RemoveCar()
+    {
+        decimal initialPrice, priceByHour, totalHoursParked;
+        string car;
+
+        Console.WriteLine("Enter reg number to remove car: ");
+        string regNumber = Console.ReadLine();
+
+        Console.WriteLine("Enter initial price: ");
+        initialPrice = Convert.ToDecimal(Console.ReadLine());
+
+
+        Console.WriteLine("Enter price by Hour: ");
+        priceByHour = Convert.ToDecimal(Console.ReadLine());
+
+        Console.WriteLine("Enter total hours parked: ");
+        totalHoursParked = Convert.ToDecimal(Console.ReadLine());
+
+        //logic to calculate prices
+
+        //does the math to pay
+        decimal finalPrice = initialPrice + (priceByHour * totalHoursParked);
+
+
+
+
+        //removes car from List
+        vehicles.Remove(regNumber);
+        Console.WriteLine($"Car {regNumber} removed from the system");
+
+        //display the final price to customer
+        Console.WriteLine($"Final price: {finalPrice} $");
+    }
+
+    public void DisplayCars()
+    {
+        Console.WriteLine("Displaying all registered cars: ");
+
+
+        //display car list
+        foreach (var vehicle in vehicles)
+        {
+            Console.WriteLine($"car reg {vehicle} registered into the system");
+        }
+    }
+
+
+
 }
